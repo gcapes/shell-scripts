d55244be2216ff9d821cefae21b347082ae50a5b
Gerard Capes <gerard.capes@manchester.ac.uk>
2018-10-05 13:18:37 +0100

    Auto-detect list of remotes, report when no HTTPS found
    Detect list of remotes, and give error message when there are no
    https remotes which need converting.

--- git-https-to-ssh.sh
+++ git-https-to-ssh.sh
@@ -1,27 +1,30 @@
 # Convert git repo from using HTTPS to ssh
 
 REMOTES=$(git remote -v)
-HTTP_REGEX="https://(\S+)/(\w+)/(\S+)\.git"
-for REMOTE in origin upstream
-do
-	# Generate ssh address for remote
-	REGEX=$REMOTE"\s${HTTP_REGEX}\s\(fetch\)"
+HTTPS_REGEX="https://(\S+)/(\w+)/(\S+)\.git"
+REMOTES_REGEX='(\w+) https'
+REMOTES_LIST=$(echo ${REMOTES} | grep -Eo "${REMOTES_REGEX}" | uniq | cut -d ' '  -f 1)
 
-	if [[ $REMOTES =~ $REGEX ]]
-	then
-		HTTP_REMOTE=${BASH_REMATCH[1]}
-		DOMAIN=${BASH_REMATCH[1]}
-		USER_NAME=${BASH_REMATCH[2]}
-		REPO=${BASH_REMATCH[3]}
-		SSH_REMOTE=git@${DOMAIN}:${USER_NAME}/${REPO}.git
+if [[ ! -z ${REMOTES_LIST} ]] 
+then
+	for REMOTE in ${REMOTES_LIST}
+	do
+		# Generate ssh address for remote
+		REGEX=${REMOTE}"\s${HTTPS_REGEX}\s\(fetch\)"
 
-		# Set remote url
-		SET_SSH_REMOTE="git remote set-url ${REMOTE} ${SSH_REMOTE}"
-		echo ${REMOTE} set to ${SSH_REMOTE}
-		$(${SET_SSH_REMOTE})
-	else
-		# Logic is wrong.
-		# This prints if remote doesn't exist, or if it uses ssh already.
-		echo ${REMOTE} already uses ssh
-	fi
-done
+		if [[ ${REMOTES} =~ ${REGEX} ]]
+		then
+			DOMAIN=${BASH_REMATCH[1]}
+			USER_NAME=${BASH_REMATCH[2]}
+			REPO=${BASH_REMATCH[3]}
+			SSH_REMOTE=git@${DOMAIN}:${USER_NAME}/${REPO}.git
+
+			# Set remote url
+			SET_SSH_REMOTE="git remote set-url ${REMOTE} ${SSH_REMOTE}"
+			echo ${REMOTE} set to ${SSH_REMOTE}
+			$(${SET_SSH_REMOTE})
+		fi
+	done
+else
+	echo "No https remotes found"
+fi
